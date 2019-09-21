f1ec6468ee1af1a7b347d44225ed5e8d72365f40
Alter Wang <WLAlter98@outlook.com>
2019-09-21 12:32:10 +0800

    添加示例代码

--- ControlStateSwitch.java
+++ ControlStateSwitch.java
@@ -37,7 +37,7 @@
             int i = 2;
             switch(i){
                 case 1: case 2: case 3: case 10:
-                    System.out.println("Merged cases.")  // i=1,2,3,10都会执行该分支中的语句
+                    dayOfWeek = "Mergd cases.")  // i=1,2,3,10都会执行该分支中的语句
             }
  */
 
@@ -49,7 +49,69 @@
         // switch(i){}  // 编译错误：不兼容的类型: 从long转换到int可能会有损失  
         switch((int)i){} 
 
-        byte b = 12;
-        switch(b){}
+        // 第4条
+        // byte x = 12;
+        // short x = 12;
+        char x = 12;
+        switch(x){}
+
+        // switch(true){}  // 编译错误: 不兼容的类型: boolean无法转换为int
+
+        String name = "ZhangSan";
+        switch(name){}
+
+        /* 
+            接受用户输入；1~7
+            1 -> 星期一
+            2 -> 星期二
+            ...
+            7 -> 星期天
+        */
+        java.util.Scanner s = new java.util.Scanner(System.in);
+        System.out.print("请输入数字（1~7）；");
+        int num = s.nextInt();
+        String dayOfWeek = "";
+        switch(num){  // 识别int类型数据
+            case 1:
+                dayOfWeek = "星期一";
+                break;
+            case 2:
+                dayOfWeek = "星期二";
+                break;
+            case 3:
+                dayOfWeek = "星期三";
+                break;
+            case 4:
+                dayOfWeek = "星期四";
+                break;
+            case 5:
+                dayOfWeek = "星期五";
+                break;
+            case 6:
+                dayOfWeek = "星期六";
+                break;
+            case 7:
+                dayOfWeek = "星期天";
+                break;
+            default:
+                dayOfWeek = "错误";
+                break;
+        }
+        // System.out.println(dayOfWeek);
+        
+        // case穿透，合并，因为存在穿透，所以可以合并
+        String dayOfWorkOrRest = "";
+        switch(dayOfWeek){  // 识别Sting类型数据
+            case "星期一": case "星期二": case "星期三": case "星期四": case "星期五":
+                dayOfWorkOrRest = "工作日";
+                break;
+            case "星期六": case "星期天":
+                dayOfWorkOrRest = "休息日";
+                break;
+            default:
+                dayOfWorkOrRest = "输入不在指定范围";
+                break;
+        }
+        System.out.println(dayOfWeek+"："+dayOfWorkOrRest);
     }
  }
