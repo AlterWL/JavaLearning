ddd0ad028d7a850dc1df20f91009b8f32fea2f17
Alter Wang <WLAlter98@outlook.com>
2019-09-23 15:51:54 +0800

    2）continue语句

--- StatementBreakContinue.java
+++ StatementBreakContinue.java
@@ -6,12 +6,20 @@
         - break语句可以用在循环语句中，用来终止循环的执行；
         - break只终止当前所在循环【离他最近的循环语句】。 
 
+    2、continue语句：
+        - continue表示：继续/go on/下一个；
+        - 同break一样，continue可以+";" 成为一个单独完整的java语句：“continue;”，主要出现在循环当中用以控制循环的执行；
+    
+    3、break和continue的区别：
+        - break表示循环不再执行，终止循环；
+        - continue表示直接进入下一次循环继续执行。
  */
 
 public class StatementBreakContinue
 {
     public static void main(String[] args)
     {
+        System.out.println("=================== break语句 ===================");
         for(int i=1;i<5;i++)
         {
             int j = 1;
@@ -24,7 +32,7 @@ public class StatementBreakContinue
             }
             System.out.println();
         }
-
+        
         System.out.println("-----------------------");
         // 一下语法较少使用：break终止指定循环
         F1:for(int i=1;i<5;i++)  // 给for循环起名F1
@@ -37,6 +45,35 @@ public class StatementBreakContinue
                     break F1;  // 终止F1循环
                 System.out.print(i*j+" ");
             }
-        }        
+        }
+        System.out.println();  
+
+        System.out.println("================== continue语句 ==================");
+        for(int i=1;i<10;i++)
+        {
+            if(i == 5)
+            {
+                // break;  // 输出1 2 3 4 
+                continue;  // 输出1 2 3 4 6 7 8 9 
+            }
+            System.out.print(i+" ");
+            // System.out.println();
+        }
+        System.out.println(); 
+
+        System.out.println("-----------------------");
+        // 与break类似，continue可指定循环
+        for1:for(int i=1;i<5;i++)  // 给for循环起名for1
+        {
+            for2:for(int j=1;j<5;j++)  // 给for循环起名for2
+            {
+                if(j == 4)
+                {
+                    System.out.println(); 
+                    continue for1;  // 跳到for1的下一次循环
+                }
+                System.out.print(i*j+" ");
+            }
+        } 
     }
 }
