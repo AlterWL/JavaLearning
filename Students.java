f227cbc2aa073d1e00ba7908fee08ffbe67a0501
Alter Wang <WLAlter98@outlook.com>
2019-09-27 02:22:57 +0800

    类的定义举例：学生类

---
+++ Students.java
@@ -0,0 +1,14 @@
+public class Student  // 定义一个公开类，是一个学生对象的模板
+{
+    // 属性-描述对象的状态信息
+    // 属性通常采用变量的方式定义
+    // 在类体中，方法体之外的变量称为“成员变量”，默认值向0看齐
+
+    String name;  // 姓名
+    int number;  // 学号
+    boolean gender;  // 性别
+    int age;  // 年龄
+    String grade;  // 年级
+
+    // 方法-描述对象的动作信息
+}
