08fa38cddf880744425fd14f6a10856442be2c31
Alter Wang <WLAlter98@outlook.com>
2019-09-27 19:05:31 +0800

    添加类属性和测试过程： 1、给学生类添加Computer类 2、给学生对象添加电脑对象

--- StudentTest.java
+++ StudentTest.java
@@ -17,6 +17,8 @@
 // 学生测试类
 public class StudentTest
 {
+    // 每一个类中都可以编写主方法，但一般情况下，一个系统只有一个入口，所以主方法一般只写一个
+    // 所以Student类中不需要编写主方法
     public static void main(String[] args)
     {
         Student s = new Student();  // s是new所创建的对象的引用，即保存的是在堆内存中保存的对象的地址
@@ -25,17 +27,60 @@ public class StudentTest
         // 读取：引用.变量名;
         // 修改：引用.变量名 = 值;
         System.out.println("--------- 初始值 ---------");
-        System.out.println("姓名 ->"+s.name+"\n学号 ->"+s.number+"\n性别 ->"+(s.gender?"男":"女"));
+        System.out.println("姓名 ->"+s.name+"\n学号 ->"+s.number+"\n年龄 ->"+s.age+"\n性别 ->"+(s.gender?"男":"女"));
         s.name = "阿羡";
         s.number = 2019123;
+        s.age = 20;
         s.gender = true;
         System.out.println("--------- 修改后 ---------");
-        System.out.println("姓名 ->"+s.name+"\n学号 ->"+s.number+"\n性别 ->"+(s.gender?"男":"女"));
-
+        System.out.println("姓名 ->"+s.name+"\n学号 ->"+s.number+"\n年龄 ->"+s.age+"\n性别 ->"+(s.gender?"男":"女"));
         // System.out.println(Student.name);  // 编译错误: 无法从静态上下文中引用非静态 变量 name
 
-        s = null;
+        // s = null;
         // 空引用访问实例相关的数据，编译通过因为符合语法， 运行时发生异常
-        System.out.println(s.name);  // 运行时发生空指针异常：java.lang.NullPointerException
+        // System.out.println(s.name);  // 运行时发生空指针异常：java.lang.NullPointerException
+
+        System.out.println("--------- 电脑对象 ---------");
+        // System.out.println(s.name+"的电脑颜色是"+s.computer.color);  // java.lang.NullPointerException
+        Computer laptop = new Computer();
+        laptop.brand = "ASUS";
+        laptop.style = "Office";
+        laptop.color = "Red";
+        laptop.price = 5499;
+
+        s.computer = laptop;
+        System.out.println(s.name+"的电脑颜色是"+s.computer.color);
     }
+}
+
+class Student  // 定义一个类，是一个学生对象的模板
+{
+    // 属性-描述对象的状态信息
+    // 属性通常采用变量的方式定义
+    // 在类体中，方法体之外的变量称为“成员变量”，默认值向0看齐
+    // 成员变量不能通过类直接访问，需要先创建对象，在通过对象去访问。因为在创建对象之后，变量才会被分配内存空间的。因此，这种成员变量又称为“实例变量”/“对象变量”、
+    // 成员变量分为实例变量前面修饰符没有static】和静态变量【前面修饰符有static】
+
+    // 实例变量
+    String name;  // 姓名
+    int number;  // 学号
+    boolean gender;  // 性别
+    int age;  // 年龄
+    String grade;  // 年级
+
+    Computer computer;  // 学生的电脑
+
+    // 方法-描述对象的动作信息
+    public void study()
+    {
+        System.out.println("I'm studying now.");
+    }
+}
+
+class Computer  // 定义一个公开类，是一个学生对象的模板
+{
+    String brand;  // 品牌
+    String style;  // 款式
+    String color;  // 颜色
+    int price;  // 价格
 }
