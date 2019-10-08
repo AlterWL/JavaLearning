18b5d0ef009373a33bffd30de10f225a36e9de1d
Alter Wang <WLAlter98@outlook.com>
2019-10-08 23:58:15 +0800

    添加HongKongPeople类，继承Chinese类

---
+++ HongKongPeople.java
@@ -0,0 +1,11 @@
+/**
+ * HongKongPeople
+ */
+public class HongKongPeople extends Chinese {
+
+    private String nativePlace = country+"香港"; // 籍贯
+
+    public HongKongPeople() {
+        super();
+    }
+}
