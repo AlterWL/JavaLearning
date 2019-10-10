3ed551350aa127d8ac4b02ebd913603c1e566fac
Alter Wang <WLAlter98@outlook.com>
2019-10-11 00:01:42 +0800

    创建基类：动物类

---
+++ Animal.java
@@ -0,0 +1,15 @@
+/**
+ * Animal
+ * 
+ * 父类，动物类
+ * 
+ */
+public class Animal {
+
+    public static void main(String[] args) {
+        
+        public void move() {
+            System.out.println("动物在移动...");
+        }
+    }
+}
