13bdab93572972a6e77c53c6734ca4443fa9a124
Alter Wang <WLAlter98@outlook.com>
2019-10-11 00:02:12 +0800

    创建子类：猫类

---
+++ cat.java
@@ -0,0 +1,12 @@
+/**
+ * cat
+ */
+public class cat extends Animal{
+
+    public static void main(String[] args) {
+        
+        public void move() {
+            System.out.println("猫在走猫步...");
+        }
+    }
+}
