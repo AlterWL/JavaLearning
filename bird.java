f2bbec802605631350dd61bcea5af2b8c7b21eae
Alter Wang <WLAlter98@outlook.com>
2019-10-11 00:01:59 +0800

    创建子类：鸟类

---
+++ bird.java
@@ -0,0 +1,12 @@
+/**
+ * bird
+ */
+public class bird extends Animal {
+
+    public static void main(String[] args) {
+        
+        public void move() {
+            System.out.println("鸟在空中飞...");
+        }
+    }
+}
