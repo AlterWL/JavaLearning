58636f85444758534baafc4b3ada7d0600cc6bca
Alter Wang <WLAlter98@outlook.com>
2019-10-11 00:49:04 +0800

    对Animal类更正错误： 方法中不能再定义方法，直接删除了主方法。

--- Animal.java
+++ Animal.java
@@ -5,11 +5,8 @@
  * 
  */
 public class Animal {
-
-    public static void main(String[] args) {
-        
-        public void move() {
-            System.out.println("动物在移动...");
-        }
+     
+    public void move() {
+        System.out.println("动物在移动...");
     }
 }
