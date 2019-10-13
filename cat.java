4ff8e142217f71978c974af1b69383ab65ee2e65
Alter Wang <WLAlter98@outlook.com>
2019-10-14 00:45:21 +0800

    1、添加了构造方法 2、修改了eat方法

--- cat.java
+++ cat.java
@@ -3,8 +3,16 @@
  * 
  * 子类：猫类 - 继承自动物类
  */
-public class Cat extends Animal{
+public class Cat extends Pet{
 
+    // 默认名字叫“粉粉”
+    public Cat() {
+        this("荣荣");
+    }
+    // 给宠物狗取个名字
+    public Cat(String name) {
+        setName(name);
+    }
         
     // 重写从父类中继承过来的方法
     public void move() {
@@ -16,7 +24,9 @@ public class Cat extends Animal{
         System.out.println("猫在抓老鼠...");
     }
 
+    // 可以给猫吃鱼
     public void eat(String food) {
-        System.out.println("猫正在吃"+food+"...");
+        System.out.print("(小猫)");
+        super.eat(food);
     }
 }
